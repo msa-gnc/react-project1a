1f17133a6dcc178283b458e829b2593adaa5db85
msa.gnc <musagenc167@gmail.com>
2024-12-24 22:34:31 +0300

    add folder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import react from 'react';
+import './App.css';
+import helloreact from "./assets/react.png"
+
+function App() {
+  return (
+    <div className="App">
+    <p>Merhaba</p>
+    <img src={react} alt=""  className="helloPicture" />
+    </div>
+  );
+}
+
+
+export default App;
